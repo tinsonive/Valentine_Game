5f395b245b0d988628e0cc98319733326015a260
Tinson <66818769+tinsonive@users.noreply.github.com>
2025-02-03 16:23:23 +0800

    Init Commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "HKIIT at IVE(LWL)-CNY Firecracker Wish!-1.0";
+const contentToCache = [
+    "Build/Valentine_Game.loader.js",
+    "Build/Valentine_Game.framework.js",
+    "Build/Valentine_Game.data",
+    "Build/Valentine_Game.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
